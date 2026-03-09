4979b9de3a3ad07e0a0956f4fae60968c288cf25
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2026-03-09 12:01:25 +0530

    Add EvaluationResult Support + Helper Methods
    - Update Test Cases
    - Also add `setup_evaluation_environment()` to be ran at the start for setting up env.

--- src/evaluation/test_evaluation_package.py
+++ src/evaluation/test_evaluation_package.py
@@ -17,9 +17,21 @@ assert test_installation(), "SWE-bench installation test failed."
 print("SWE-bench installation test passed. ✅")
 
 print("Get Validation Test Case Results...")
-results = get_test_case_results(instance_id=VALIDATION_INSTANCE_ID, run_id=VALIDATION_RUN_ID, predictions_path=VALIDATION_PREDICTIONS_PATH)
-assert results["method_success"], f"Failed to get test case results: {results.get('error')}"
-assert results.get("test_case_passed"), "Validation test case did not pass."
+bug_results = is_bug_resolved(instance_id=VALIDATION_INSTANCE_ID, run_id=VALIDATION_RUN_ID, predictions_path=VALIDATION_PREDICTIONS_PATH)
+
+assert bug_results["method_success"], f"Failed to get test case results: {bug_results.get('error')}"
+assert bug_results.get("test_case_passed"), "Validation test case did not pass."
+
+report_path = get_report_path(instance_id=VALIDATION_INSTANCE_ID, run_id=VALIDATION_RUN_ID, predictions_path=VALIDATION_PREDICTIONS_PATH)
+assert report_path.exists() and report_path.is_file(), "Report file not found after validation run."
+
+report_data = get_report_file(report_path)
+assert report_data["method_success"], f"Failed to read report file: {report_data.get('error')}"
+
+test_case_results = get_test_case_results(report_data["report_data"])
+assert test_case_results['bug_resolved'], "Bug was not resolved according to test case results."
+assert test_case_results['pass_to_pass_success'], "PASS_TO_PASS test case did not pass."
+assert test_case_results['fail_to_pass_success'], "FAIL_TO_PASS test case did not pass."
 print("Validation test case passed successfully. ✅")
 
 print("Cleaning up validation logs and results...")
@@ -32,4 +44,4 @@ print("Evaluation directory cleaned up successfully. ✅")
 
 # To test "run_evaluation_on_instance"
 
-print("SWE-bench is successfully set up and ready for evaluation. [Tests passed.]")
+print("SWE-bench is successfully set up and ready for evaluation. [Tests passed ✅.]")
